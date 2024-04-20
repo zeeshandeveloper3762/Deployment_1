e1fe091df562cfd7b716bfc96d7af0a820cabc38
zk3762100@gmail.com <132770259+zeeshandeveloper3762@users.noreply.github.com>
2024-04-20 20:27:15 +0500

    add get url in server.js file

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    success: true,
+    message: "Hello World",
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
